64a6e587f4eda6e0fdceaf0253f811bf0eb0f830
bsnbk <beysenbek@gmail.com>
2018-02-12 18:07:56 +0600

    'upd'

---
+++ PP2_W6D1/Snake/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.SetWindowSize(Game.boardW, Game.boardH);
+            Console.SetBufferSize(Game.boardW, Game.boardH);
+            Console.CursorVisible = false;
+
+
+            StatusBar.ShowInfo("test!");
+
+
+            while (true) { }
+
+            /*Menu menu = new Menu();
+            menu.Draw();
+            /*Game game = new Game();
+            game.SetupBoard();
+            game.Start();
+
+            while (true)
+            {
+                ConsoleKeyInfo pressedButton = Console.ReadKey();
+                menu.Process(pressedButton);
+                //game.Process(pressedButton);
+            }
+            */
+        }
+    }
+}
---
+++ PP2_W6D1/Thread/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine(System.Threading.Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Hi!");*/
+
+            ThreadStart ts = new ThreadStart(DoIt);
+            System.Threading.Thread t = new System.Threading.Thread(ts);
+            t.Start();
+
+            ThreadStart ts2 = new ThreadStart(DoIt2);
+            System.Threading.Thread t2 = new System.Threading.Thread(ts2);
+            t2.Start();
+
+            //DoIt();
+        }
+
+        private static void DoIt()
+        {
+            while (true)
+            {
+                Console.WriteLine("Hello world!");
+                System.Threading.Thread.Sleep(1000);
+            }
+            //Console.WriteLine(System.Threading.Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private static void DoIt2()
+        {
+            while (true)
+            {
+                Console.WriteLine("Hi!");
+                System.Threading.Thread.Sleep(2000);
+            }
+            //Console.WriteLine(System.Threading.Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
