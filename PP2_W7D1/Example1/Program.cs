1eca709c58aae6fe3ad7b3f36d546500d31ab8b0
bsnbk <beysenbek@gmail.com>
2018-02-19 17:41:57 +0600

    'upd'

---
+++ PP2_W7D1/Example1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    abstract class GameObject
+    {
+        public void Save()
+        {
+            File.Create(this.GetType().Name + ".xml");
+        }
+    }
+    class Worm:GameObject
+    {
+
+    }
+
+    class Food:GameObject
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worm w = new Worm();
+            Food f = new Food();
+            w.Save();
+            f.Save();
+        }
+    }
+}
---
+++ PP2_W7D1/Example2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread tr = null;
+
+            while (true)
+            {
+                ConsoleKeyInfo pressedButton = Console.ReadKey();
+
+                switch (pressedButton.Key)
+                {
+                    case ConsoleKey.Spacebar:
+                        tr.Abort();
+                        break;
+                    case ConsoleKey.Enter:
+                        tr = new Thread(new ThreadStart(DoIt));
+                        tr.Start();
+                        break;
+                }
+            }
+            
+        }
+
+        static void DoIt()
+        {
+            while (true)
+            {
+                Console.WriteLine(DateTime.Now.Ticks);
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
