b8db1025b4214f5019334502a7976275979a1dd5
bsnbk <beysenbek@gmail.com>
2018-02-26 17:23:23 +0600

    'upd'

---
+++ PP2_W8D1/Snake/Game.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Snake
+{
+    enum GameLevel
+    {
+        First,
+        Second,
+        Bonus
+    }
+
+    class Game
+    {
+        Timer wormTimer;
+
+        public static int boardW = 35;
+        public static int boardH = 35;
+
+        bool[,] field = new bool[10,10];
+
+        public Worm worm;
+        public Food food;
+        public Wall wall;
+
+        GameLevel gameLevel;
+
+        public void SetupBoard()
+        {
+            Console.SetWindowSize(Game.boardW, Game.boardH);
+            Console.SetBufferSize(Game.boardW, Game.boardH);
+            Console.CursorVisible = false;
+        }
+
+        public Game()
+        {
+            gameLevel = GameLevel.First;
+         
+            worm = new Worm(new Point { X = 10, Y = 10 }, 
+                            ConsoleColor.White, '*');
+            food = new Food(new Point { X = 14, Y = 10 },
+                           ConsoleColor.White, '+');
+            wall = new Wall(null,ConsoleColor.White, '#');
+
+            wall.LoadLevel(GameLevel.First);
+
+
+            wormTimer = new Timer();
+            wormTimer.Interval = 500;
+            wormTimer.Elapsed += T_Elapsed;
+        }
+
+        private void T_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            worm.Clear();
+            worm.Move();
+            CanWormEat();
+            IsItCrash();
+            worm.Draw();
+        }
+
+        private void CanWormEat()
+        {
+            if (worm.body[0].Equals(food.body[0]))
+            {
+                worm.body.Add(new Point { X = food.body[0].X, Y = food.body[0].Y });
+                food.Draw();
+            }
+        }
+        private void IsItCrash()
+        {
+            foreach (Point p in wall.body)
+            {
+                if (p.Equals(worm.body[0]))
+                {
+                    wormTimer.Stop();
+                    worm.IsAlive = false;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Clear();
+                    Console.WriteLine("GAME OVER!!!");
+                  
+                    break;
+                }
+            }
+        }
+
+        public void Exit()
+        {
+
+        }
+
+        public void Start()
+        {
+            wall.Draw();
+            food.Draw();
+            wormTimer.Start();
+        }
+
+        public void Process(ConsoleKeyInfo pressedButton)
+        {
+            switch (pressedButton.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    worm.DX = 0;
+                    worm.DY = -1;
+                    break;
+                case ConsoleKey.DownArrow:
+                    worm.DX = 0;
+                    worm.DY = 1;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    worm.DX = -1;
+                    worm.DY = 0;
+                    break;
+                case ConsoleKey.RightArrow:
+                    worm.DX = 1;
+                    worm.DY = 0;
+                    break;
+                case ConsoleKey.Escape:
+                    break;
+                case ConsoleKey.Spacebar:
+                    if (worm.IsPaused)
+                    {
+                        worm.IsPaused = false;
+                        wormTimer.Start();
+                    }
+                    else
+                    {
+                        worm.IsPaused = true;
+                        wormTimer.Stop();
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ PP2_W8D1/Timer2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Timer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer t = new Timer();
+            t.Interval = 1000;
+            t.Elapsed += DoIt;
+            t.Start();
+
+            Timer t2 = new Timer();
+            t2.Interval = 2000;
+            t2.Elapsed += DoIt2;
+            t2.Start();
+
+            while (true)
+            {
+
+            }
+
+            t.Stop();
+        }
+
+        private static void DoIt(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("ok");
+
+        }
+
+        private static void DoIt2(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("ok2");
+        }
+    }
+}
