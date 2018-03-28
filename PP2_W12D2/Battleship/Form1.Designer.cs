3d2fb3bf494fadd0fdd8f351a7303e110b097ca4
bsnbk <beysenbek@gmail.com>
2018-03-28 18:16:01 +0600

    'upd'

---
+++ PP2_W12D2/Battleship/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Battleship
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.player1Panel = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // player1Panel
+            // 
+            this.player1Panel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.player1Panel.Location = new System.Drawing.Point(37, 54);
+            this.player1Panel.Name = "player1Panel";
+            this.player1Panel.Size = new System.Drawing.Size(300, 300);
+            this.player1Panel.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(482, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.gameToolStripMenuItem.Text = "Game";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 453);
+            this.Controls.Add(this.player1Panel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel player1Panel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ PP2_W12D2/Battleship/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Battleship
+{
+    public partial class Form1 : Form
+    {
+        Brain brain;
+        int cellW = 30;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < 10; ++i)
+            {
+                for (int j = 0; j < 10; ++j)
+                {
+                    Button btn = new Button();
+                    btn.Name = i + "_" + j;
+                    btn.Click += Btn_Click;
+                    btn.Size = new Size(cellW, cellW);
+                    btn.Location = new Point(i * cellW, j * cellW);
+                    player1Panel.Controls.Add(btn);
+                }
+            }
+
+            brain = new Brain(ChangeButton);
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            brain.Process(btn.Name);
+        }
+
+        private int GetIndexByName(string name)
+        {
+            Random rnd = new Random();
+            return rnd.Next(0, player1Panel.Controls.Count);
+        }
+        private void ChangeButton(CellState[,] map)
+        {
+            Color colorToFill = Color.White;
+
+            for(int i = 0; i < 10; ++i)
+            {
+                for(int j = 0; j < 10; ++j)
+                {
+                    switch (map[i,j])
+                    {
+                        case CellState.empty:
+                            colorToFill = Color.White;
+                            break;
+                        case CellState.busy:
+                            colorToFill = Color.Blue;
+                            break;
+                        case CellState.striked:
+                            colorToFill = Color.Yellow;
+                            break;
+                        case CellState.missed:
+                            colorToFill = Color.Gray;
+                            break;
+                        case CellState.killed:
+                            colorToFill = Color.Red;
+                            break;
+                        case CellState.masked:
+                            colorToFill = Color.DarkGray;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    player1Panel.Controls[10 * i + j].BackColor = colorToFill;
+                }
+            }
+            
+            
+        }
+    }
+}
