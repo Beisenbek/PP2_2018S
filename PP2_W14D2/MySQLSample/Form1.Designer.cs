b63cfecb5c67c0dee80dab3d3441ee279f50ea3d
bsnbk <beysenbek@gmail.com>
2018-04-11 18:03:02 +0600

    'upd'

---
+++ PP2_W14D2/MySQLSample/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace MySQLSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database1DataSet = new MySQLSample.Database1DataSet();
+            this.tableTableAdapter = new MySQLSample.Database1DataSetTableAdapters.TableTableAdapter();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableBindingSource
+            // 
+            this.tableBindingSource.DataMember = "Table";
+            this.tableBindingSource.DataSource = this.database1DataSet;
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tableTableAdapter
+            // 
+            this.tableTableAdapter.ClearBeforeFill = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(205, 210);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 287);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.BindingSource tableBindingSource;
+        private Database1DataSetTableAdapters.TableTableAdapter tableTableAdapter;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PP2_W14D2/MySQLSample2/Form1.cs
@@ -0,0 +1,60 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySQLSample2
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection mySqlConnection;
+        MySqlDataAdapter mySqlDataAdapter;
+        MySqlCommandBuilder mySqlCommandBuilder;
+        DataTable dataTable;
+        BindingSource bindingSource;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mySqlConnection = new MySqlConnection(
+               "SERVER=localhost;" +
+               "DATABASE=fitdb;" +
+               "UID=bsnbk;" +
+               "PASSWORD=1234;");
+            mySqlConnection.Open();
+
+            string query = "SELECT * FROM Customers2";
+
+            mySqlDataAdapter = new MySqlDataAdapter(query, mySqlConnection);
+            mySqlCommandBuilder = new MySqlCommandBuilder(mySqlDataAdapter);
+
+            mySqlDataAdapter.UpdateCommand = mySqlCommandBuilder.GetUpdateCommand();
+            mySqlDataAdapter.DeleteCommand = mySqlCommandBuilder.GetDeleteCommand();
+            mySqlDataAdapter.InsertCommand = mySqlCommandBuilder.GetInsertCommand();
+
+            dataTable = new DataTable();
+            mySqlDataAdapter.Fill(dataTable);
+
+            bindingSource = new BindingSource();
+            bindingSource.DataSource = dataTable;
+
+            dataGridView1.DataSource = bindingSource;
+            bindingNavigator1.BindingSource = bindingSource;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mySqlDataAdapter.Update(dataTable);
+        }
+    }
+}
